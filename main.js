dc73f5acf9abba2de19c77aa568050ac5a834a14
John Murphy <murf2073@gmail.com>
2017-10-09 15:23:32 -0400

    updated script tags and added other pizzas

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector("submit[id=submit-pep]").addEventListener("click", function(e){
+	document.getElementById("submit-pep").addEventListener("click", function(e){
 		e.preventDefault();
-		let temp = document.querySelector("input[name=pep]").value;
+		let temp = document.getElementById("pep").value;
 		Cookies.set("PepperoniPizzas", temp);
 		changePep();
 	});
+	document.getElementById("submit-plain").addEventListener("click", function(e){
+		e.preventDefault();
+		let temp = document.getElementById("plain").value;
+		Cookies.set("PlainPizzas", temp);
+		changePlain();
+	});
+	document.getElementById("submit-saus").addEventListener("click", function(e){
+		e.preventDefault();
+		let temp = document.getElementById("saus").value;
+		Cookies.set("SausagePizzas", temp);
+		changeSaus();
+	});	
 });
 
 function changePep(){
-	let total = Cookies.get("Pepperoni Pizzas");
+	let total = Cookies.get("PepperoniPizzas");
 	document.getElementById("count-pep").innerHTML = `${total}`;
+}
+
+function changePlain(){
+	let total = Cookies.get("PlainPizzas");
+	document.getElementById("count-plain").innerHTML = `${total}`;
+}
+
+function changeSaus(){
+	let total = Cookies.get("SausagePizzas");
+	document.getElementById("count-saus").innerHTML = `${total}`;
 }
